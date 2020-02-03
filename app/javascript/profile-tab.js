2d7ca708f6ff0d522b6ba7cba329818e0a2134e6
Kevin <kaplunkett@gmail.com>
2020-02-03 16:50:55 -0500

    visual updates

---
+++ app/javascript/profile-tab.js
@@ -0,0 +1,41 @@
+$(function () {
+    console.log("Hello from profile-tab.js");
+	$('.profile__stories').show();
+    $('.profile__friends').hide();
+	$('.profile__conversations').hide();
+
+
+	$('.profile__tab').click(function () {
+		console.log("click");
+	});
+
+	$('.profile__stories_tab').click(function () {
+		$('.profile__stories_tab').addClass("profile__tab_active");
+		$('.profile__friends_tab').removeClass("profile__tab_active");
+		$('.profile__conversations_tab').removeClass("profile__tab_active");
+
+		$('.profile__stories').show();
+		$('.profile__friends').hide();
+		$('.profile__conversations').hide();
+	});
+
+	$('.profile__friends_tab').click(function () {
+		$('.profile__stories_tab').removeClass("profile__tab_active");
+		$('.profile__friends_tab').addClass("profile__tab_active");
+		$('.profile__conversations_tab').removeClass("profile__tab_active");
+
+		$('.profile__stories').hide();
+		$('.profile__friends').show();
+		$('.profile__conversations').hide();
+	});
+
+	$('.profile__conversations_tab').click(function () {
+		$('.profile__stories_tab').removeClass("profile__tab_active");
+		$('.profile__friends_tab').removeClass("profile__tab_active");
+		$('.profile__conversations_tab').addClass("profile__tab_active");
+
+		$('.profile__stories').hide();
+		$('.profile__friends').hide();
+		$('.profile__conversations').show();
+	});
+});
